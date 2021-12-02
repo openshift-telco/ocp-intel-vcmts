4fae5f163d79063b4b1eb04f0c9a30af32ce5f98
Federico (tele) Rossi <tele0x@gmail.com>
2021-12-02 08:25:50 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# Intel vCMTS on OpenShift
+
+## Table of Contents
+
+<!-- TOC -->
+- [Intel vCMTS on OpenShift](#intel-vcmts-on-openshift)
+  - [Introduction](#introduction)
+  - [Prerequisities](#prerequisities)
+  - [Architecture](#architecture)
+  - [Build Container Images](#build-container-images)
+    - [vCMTS](#vcmts)
+    - [Pktgen](#pktgen)
+  - [Installation](#installation)
+<!-- TOC -->
+
+## Introduction
+
+This document describes how to build, install and run the Intel vCMTS reference dataplane system on Red Hat OpenShift Container Platform. Intel vCMTS includes a DPDK Pktgen based cable traffic generation system for upstream and downstream traffic simulation.
+
+## Prerequisities
+
+  - Red Hat OpenShift version 4.9
+  - Podman
+  - Buildah
+  - Helm3
+  - Intel vCMTS Package
+
+## Architecture
+
+## Build Container Images
+
+Clone this repository.
+
+```
+$ git clone https://github.com/openshift-telco/ocp-intel-cvmts
+```
+
+Acquire Intel vCMTS package and copy to `ocp-intel-vcmts` directory. Example package: `intel-vcmtsd-v21-10-0-beta.tar`
+Edit `build_config` and adjust as needed according to required build versions.
+Example:
+
+```
+$ cat build_config 
+# Intel VCMTS package version
+VCMTS_VERSION="21.10.0-beta"
+# Build directory
+VCMTS_ROOT="/usr/src/vcmts"
+
+# Change version with corresponding Intel VCMTS build requirements
+DPDK_VERSION="21.02"
+DPDK_VERSION_PKTGEN="20.08"
+IPSEC_MB_VERSION="0.55"
+COLLECTD_VERSION="5.12.0"
+PKTGEN_VERSION="19.10.0"
+GRAFANA_VERSION="7.2.0"
+PROMETHEUS_VERSION="2.0.0"
+```
+
+### vCMTSD
+
+Launch build script.
+
+```
+$ ./build_vcmts.sh
+```
+
+Wait until the script finishes and container is commited to local storage.
+
+```
+...
+Copying blob 657d43ff9b17 done  
+Copying config 7ee089cfa5 done  
+Writing manifest to image destination
+Storing signatures
+7ee089cfa5b61f58817b4b9c3b9e5e2f7bc5f5ca581cfa00ece671f2077a17b9
+```
+
+Verify image exists.
+
+```
+$ podman images | grep vcmts
+localhost/vcmts                                        latest       7ee089cfa5b6  50 seconds ago  259 MB
+```
+
+### Pktgen
+
+Launch build script.
+
+```
+$ ./build_pktgen.sh
+```
+
+Wait until the script finishes and container is commited to local storage.
+Verify image exists
+
+```
+$ podman images | grep pktgen
+localhost/vcmts-pktgen                                        latest       6e52dc9b5abe  50 seconds ago  2.14 GB
+```
---
+++ build_pktgen.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+##
+## Container build for PKTGEN to simulate traffic
+##
+
+# Exit script on first error
+set -o errexit
+
+# Load config
+. build_config
+
+# Use UBI as base pktgen_dev image
+pktgen_dev=$(buildah --name vcmts-pktgen-dev from registry.access.redhat.com/ubi8/ubi)
+
+# Set envs
+buildah config --env PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/local/lib64/pkgconfig $pktgen_dev
+buildah config --env VCMTS_ROOT=${VCMTS_ROOT}/vcmts $pktgen_dev
+buildah config --env RTE_TARGET=x86_64-native-linuxapp-gcc $pktgen_dev
+buildah config --env RTE_SDK=${VCMTS_ROOT}/dpdk $pktgen_dev
+
+echo -e "Copy and uncompress Intel VCMTS package"
+buildah run $pktgen_dev mkdir ${VCMTS_ROOT}
+
+buildah copy $pktgen_dev intel-vcmtsd-v${VCMTS_VERSION//./-}.tar ${VCMTS_ROOT}
+buildah config --workingdir ${VCMTS_ROOT} $pktgen_dev
+buildah run $pktgen_dev tar xvf intel-vcmtsd-v${VCMTS_VERSION//./-}.tar
+ 
+echo -e "Install packages and dependencies"
+buildah run $pktgen_dev dnf install -y --nogpgcheck --disableplugin=subscription-manager git golang gcc-c++ gcc cmake make automake autoconf bzip2 patch libtool openssl-devel python3-pip wget xz
+buildah run $pktgen_dev dnf install -y --nogpgcheck --disableplugin=subscription-manager http://mirror.centos.org/centos/8/PowerTools/x86_64/os/Packages/ninja-build-1.8.2-1.el8.x86_64.rpm http://mirror.centos.org/centos/8-stream/PowerTools/x86_64/os/Packages/meson-0.55.3-3.el8.noarch.rpm
+buildah run $pktgen_dev dnf install -y --nogpgcheck --disableplugin=subscription-manager http://mirror.centos.org/centos/8/BaseOS/x86_64/os/Packages/jansson-2.11-3.el8.x86_64.rpm http://mirror.centos.org/centos/8/AppStream/x86_64/os/Packages/jansson-devel-2.11-3.el8.x86_64.rpm
+buildah run $pktgen_dev dnf install -y --nogpgcheck --disableplugin=subscription-manager http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/numactl-libs-2.0.12-11.el8.x86_64.rpm http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/numactl-devel-2.0.12-11.el8.x86_64.rpm
+# libpcap and lua needed to compile pktgen
+buildah run $pktgen_dev dnf install -y --nogpgcheck --disableplugin=subscription-manager http://mirror.centos.org/centos/8-stream/PowerTools/x86_64/os/Packages/libpcap-devel-1.9.1-5.el8.x86_64.rpm http://mirror.centos.org/centos/8-stream/PowerTools/x86_64/os/Packages/lua-devel-5.3.4-12.el8.x86_64.rpm
+buildah run $pktgen_dev pip3 install pyelftools
+buildah run $pktgen_dev dnf clean all
+buildah run $pktgen_dev rm -fr /var/cache/dnf
+
+echo -e "Build DPDK"
+buildah config --workingdir ${VCMTS_ROOT} $pktgen_dev
+buildah run $pktgen_dev git clone http://dpdk.org/git/dpdk
+buildah config --workingdir ${VCMTS_ROOT}/dpdk $pktgen_dev
+buildah run $pktgen_dev git stash
+buildah run $pktgen_dev git clean -df -e build -e install
+buildah run $pktgen_dev git checkout main
+buildah run $pktgen_dev git pull
+buildah run $pktgen_dev git checkout v${DPDK_VERSION_PKTGEN}
+# patch ifcvfi.h, VIRTIO_F_IOMMU_PLATFORM already defined error
+buildah run $pktgen_dev sh -c "sed -e 's/\(#define VIRTIO_F_IOMMU_PLATFORM\t\t33\)/\/*\1*\//g' -i ${VCMTS_ROOT}/dpdk/drivers/vdpa/ifc/base/ifcvf.h"
+
+buildah run $pktgen_dev make install T=x86_64-native-linuxapp-gcc -j 11 MAKE_PAUSE=n
+
+echo -e "Uncompress traffic profiles"
+buildah config --workingdir ${VCMTS_ROOT}/vcmts/traffic-profiles $pktgen_dev
+
+buildah run $pktgen_dev tar -xjvf intel-vcmtsd-imix-tp-${VCMTS_VERSION}.tar.bz2
+buildah run $pktgen_dev tar -xjvf intel-vcmtsd-fixedsz-tp-${VCMTS_VERSION}.tar.bz2
+buildah config --workingdir ${VCMTS_ROOT} $pktgen_dev
+echo -e "Build pktgen-dpdk app"
+buildah run $pktgen_dev git clone https://dpdk.org/git/apps/pktgen-dpdk
+buildah config --workingdir ${VCMTS_ROOT}/pktgen-dpdk $pktgen_dev
+buildah run $pktgen_dev git stash
+buildah run $pktgen_dev git checkout master
+buildah run $pktgen_dev git pull
+buildah run $pktgen_dev git checkout pktgen-${PKTGEN_VERSION}
+buildah run $pktgen_dev git apply ${VCMTS_ROOT}/vcmts/src/patches/pktgen-${PKTGEN_VERSION}/0001-pktgen.patch
+buildah run $pktgen_dev make
+
+echo -e "Build pktgen container"
+echo -e "#########################################"
+pktgen=$(buildah --name vcmts-pktgen from registry.access.redhat.com/ubi8/ubi)
+pktgen_mount=$(buildah mount $pktgen)
+pktgen_dev_mount=$(buildah mount $pktgen_dev)
+
+buildah config --env RTE_SDK=${VCMTS_ROOT}/dpdk $pktgen
+
+buildah run $pktgen dnf install -y --nogpgcheck --disableplugin=subscription-manager libpcap http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/numactl-libs-2.0.12-11.el8.x86_64.rpm http://mirror.centos.org/centos/8-stream/BaseOS/x86_64/os/Packages/numactl-devel-2.0.12-11.el8.x86_64.rpm
+buildah run $pktgen dnf clean all
+buildah run $pktgen rm -fr /var/cache/dnf
+
+mkdir $pktgen_mount/vcmts
+mkdir $pktgen_mount/vcmts/pcaps
+# Copy files from dev container
+cp -fr ${pktgen_dev_mount}/${VCMTS_ROOT}/dpdk $pktgen_mount/vcmts/
+cp -fr ${pktgen_dev_mount}/${VCMTS_ROOT}/pktgen-dpdk $pktgen_mount/vcmts/
+cp -f  ${pktgen_dev_mount}/${VCMTS_ROOT}/pktgen-dpdk/app/x86_64-native-linuxapp-gcc/app/pktgen $pktgen_mount/vcmts/
+cp -f  ${pktgen_dev_mount}/${VCMTS_ROOT}/vcmts/pktgen/config/setup.pkt $pktgen_mount/vcmts/pcaps
+cp -fr ${pktgen_dev_mount}/${VCMTS_ROOT}/vcmts/traffic-profiles/intel-vcmtsd-fixedsz-tp-${VCMTS_VERSION}/* $pktgen_mount/vcmts/pcaps/ 
+cp -fr ${pktgen_dev_mount}/${VCMTS_ROOT}/vcmts/tools/vcmts-env/pktgen-host-config.sh $pktgen_mount/vcmts/
+
+buildah config --workingdir /vcmts $pktgen
+buildah commit $pktgen vcmts-pktgen
