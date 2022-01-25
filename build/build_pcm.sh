94d790fc422417c8915dc4ff3f0d7fdcb564f937
Alexis de Talhouët <adetalhouet89@gmail.com>
2022-01-25 16:25:51 -0500

    Fix pcm build
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- build/build_pcm.sh
+++ build/build_pcm.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 # only for local build
-# source build_config
+source build_config
 
 # Set envs
 VCMTSD_HOST=y
@@ -19,7 +19,6 @@ cp intel-vcmtsd-v${VCMTS_VERSION//./-}.tar.gz ${VCMTS_ROOT}
 tar zxvf intel-vcmtsd-v${VCMTS_VERSION//./-}.tar.gz -C ${VCMTS_ROOT}
 
 cp containerfiles/pcm-Containerfile.ubi8  $VCMTS_ROOT/vcmts/telemetry/collectd/Containerfile.ubi8
-cp patches/collectd.conf  $VCMTS_ROOT/vcmts/telemetry/collectd/collectd.conf
 cd $VCMTS_ROOT/vcmts/telemetry/collectd
 
 buildah --storage-driver vfs bud --build-arg VCMTS_VERSION=$VCMTS_VERSION -t ${IMAGE_TAG} Containerfile.ubi8
